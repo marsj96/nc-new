bd2a50df213724efdf0182784ac21bdf19b9c262
Jack <jack.oliver.marsden@gmail.com>
2021-11-22 16:06:34 +0000

    creates Nav compoenent with basic CSS

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+const Nav = () => {
+    
+    return (
+        <div>
+            <nav className="navBar"> 
+                <button className="loginButton"> 
+                    Login 
+                </button> 
+            Home Topics
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
