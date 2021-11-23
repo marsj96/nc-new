70969b22f5d0ba92b98040ef12fd01ed4c7798a8
Jack <jack.oliver.marsden@gmail.com>
2021-11-23 15:35:39 +0000

    adds article to article page and conditionally rendered post a comment button

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,36 @@
+import { useEffect } from "react"
+import { useParams } from "react-router"
+import { useContext, useState } from "react/cjs/react.development"
+import { UserContext } from "../contexts/User"
+import { getSingleArticle } from "../utils/getArticles"
+
 const Article = () => {
+
+    const [singleArticle, setSingleArticle] = useState([])
+    const { article_id } = useParams()
+    const { loggedIn } = useContext(UserContext)
+
+    useEffect(() => {
+        getSingleArticle(article_id).then((returnedArticle) => {
+            setSingleArticle(returnedArticle)
+        })
+    }, [article_id])
+
     return (
-        <h4>Article page</h4>
+    <div>
+        <div className="articlesCards" key={singleArticle.article_id}>
+            <h4 className="sTitle" key={singleArticle.title}>  {singleArticle.title} {`(${singleArticle.topic})`} </h4>
+            <p className="articleBody"> {singleArticle.body} </p>
+            <div> <p className="articleBottom"> {singleArticle.author} Votes:{singleArticle.votes} {singleArticle.created_at} </p>  </div>
+        </div>     
+        <div>
+            {loggedIn ? <button> Post a comment </button> : ""}            
+        </div>
+        <br></br>
+        <div>
+            Comments
+        </div>
+    </div>
     )
 }
 
