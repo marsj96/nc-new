43a1c21ac264c296991863876676a5e9f48c0ed9
Jack <jack.oliver.marsden@gmail.com>
2021-11-23 11:32:49 +0000

    makes filter box functional

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import { UserProidver } from './contexts/user';
 
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App">
-      <Nav />
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </div>
-    </BrowserRouter>
+    <UserProidver>
+      <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
+    </UserProidver>
   );
 }
 
