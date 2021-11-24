6135e98a4428d9f7964d6daba7806fe659661c64
Jack <jack.oliver.marsden@gmail.com>
2021-11-24 14:48:38 +0000

    adds functionality to topic to show the topic pulled from the URL

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,17 +1,28 @@
+import { useParams } from "react-router"
 import { useEffect, useState } from "react/cjs/react.development"
+import { getTopics } from "../utils/getTopic"
 
 const Topics = () => {
 
+    const filter = useParams()
+    
     const [topics, setTopics] = useState([])
 
     useEffect(()=>{
-        
-    })
+        getTopics(filter.topic).then((response)=>{
+            console.log(response)
+            setTopics(response)
+        })
+    }, [filter.topic])
 
     return (
         <div>{topics.map((topic)=>{
-            {topic.title}
-            {topic.body}
+            return (
+                <div key={topic.article_id}>
+                    {topic.title}
+                </div>
+            )
+           
         })}</div>
     )
 }
