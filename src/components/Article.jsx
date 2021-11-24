886ffcdac720d4a3beaf8ec1509b5e61b50b1837
Jack <jack.oliver.marsden@gmail.com>
2021-11-24 13:17:46 +0000

    makes comments re-render on post

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,6 +4,7 @@ import { useContext, useState } from "react/cjs/react.development"
 import { UserContext } from "../contexts/User"
 import { getSingleArticle } from "../utils/getArticles"
 import { getComments } from "../utils/getComments"
+import { patchArticleVote } from "../utils/patchArticleVotes"
 import { postSingleComment } from "../utils/postComment"
 
 const Article = () => {
@@ -14,7 +15,7 @@ const Article = () => {
     const [commentContent, setCommentContent] = useState("")
     const [userInput, setUsersInput] = useState("")
     const [comments, setComment] = useState([])
-
+    const [votes, setVotes] = useState(0)
 
     let { article_id } = useParams()
     const { loggedIn, user } = useContext(UserContext)
@@ -40,20 +41,32 @@ const Article = () => {
         getComments(article_id).then((comments)=>{
             setComment(comments)
         })
-    }, [article_id, postComment])
-
-    const sendComment = () => {
-        postSingleComment(article_id, {username: user.username, body: commentContent})
-    }
-
-    console.log(comments)
+    }, [article_id])
 
     return (
     <div>
         <div className="articlesCards" key={singleArticle.article_id}>
             <h4 className="articlesTitle" key={singleArticle.title}>  {singleArticle.title} {`(${singleArticle.topic})`} </h4>
             <p className="articleBody"> {singleArticle.body} </p>
-            <div> <p className="articleBottom"> {singleArticle.author} Votes:{singleArticle.votes} {singleArticle.created_at} </p>  </div>
+            <div> <div className="articleBottom"> {singleArticle.author} 
+            {loggedIn ? 
+            <div>
+                Votes: {singleArticle.votes + votes} <button onClick={()=>{
+                patchArticleVote(article_id, 1)
+                setPostComment(false)
+                setVotes((prevVote)=>{
+                    const newVote = prevVote
+                    return newVote+1
+                })
+            }}> up </button> <button onClick={()=>{
+                patchArticleVote(article_id, -1)
+                setVotes((prevVote)=>{
+                    const newVote = prevVote
+                    return newVote-1
+                })
+            }}> down </button>
+            </div>: ""} 
+               {singleArticle.created_at} </div>  </div>
         </div>     
         <div>
 
@@ -81,9 +94,14 @@ const Article = () => {
             </label>
             <br></br>
                 <input className="loginButton" type="submit" value="Post" onClick={()=>{
-                    sendComment()
+                    postSingleComment(article_id, {username: user.username, body: commentContent})
                     setPostComment(false)
                     setCancelPost(false)
+                    setComment((prevValue)=>{
+                        const newComments = [...prevValue]
+                        newComments.unshift({author: user.username, body: commentContent, votes: 0, created_at: "Just now..", comment_id: 500})
+                        return newComments
+                    })
                 }}/> 
             </form>
             : ""}         
@@ -95,7 +113,7 @@ const Article = () => {
                     <div className="commentCard" key={comment.comment_id}>
                     <h3 className="commentAuthor"> {comment.author} </h3>
                     <p className="commentBody"> {comment.body} </p>
-                    <p className="votesComment"> {comment.votes} </p>
+                    <p className="votesComment"> Votes:{comment.votes} {comment.created_at}</p>   
                     </div>
                 )
             })}
