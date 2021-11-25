802cd5b52c43ed09f5b13ffc6771bf62e609913a
Jack <jack.oliver.marsden@gmail.com>
2021-11-25 14:11:40 +0000

    changed filterBy to use MUI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getArticles } from "../utils/getArticles";
+import { Select } from "@mui/material";
+import { Box } from "@mui/system";
+import { NativeSelect } from "@mui/material";
+import { InputLabel } from "@mui/material";
 
 const Home = () => {
 
@@ -11,6 +15,7 @@ const Home = () => {
 
     const handleChange = (e) => {
         let {value} = e.target;
+        console.log("-------->", value)
         setSortBy(value)
         setOrder("")
     }
@@ -25,11 +30,22 @@ const Home = () => {
     <div>
         {/* filter box */}
         <div className="filterBy">
-            Filter:  
-            <select onChange={handleChange}>
-                <option>
-                    Date
-                </option>
+        <Box sx={{ minWidth: 120 }}>
+            
+            <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                Filter
+            </InputLabel>
+
+            <NativeSelect onChange={handleChange}>
+                <option value={"Title"}>Title</option>
+                <option value={"Topic"}>Topic</option>
+                <option value={"Votes"}>Votes</option>
+                <option value={"Comment_count"}>Comment count</option>
+                <option value={"Author"}>Author</option>
+            </NativeSelect>
+            
+        </Box>
+                {/* </option>
                 <option>
                     Title
                 </option>
@@ -44,8 +60,7 @@ const Home = () => {
                 </option>
                 <option>
                     Author
-                </option>
-            </select>
+                </option> */}
             <button className="ascButton" onClick={()=>{
                 setOrder("ASC")     
             }}> ↑ </button> 
@@ -91,8 +106,9 @@ const Home = () => {
                                 Votes:{`${article.votes} `}
                                 {`${article.created_at.slice(0, 10)} `}
                                 <Link to={`/articles/${article.article_id}`} style={{ textDecoration: 'none', color: 'blue' }}>
-                                Comments
+                                Comments 
                                 </Link>
+                                {` (${article.comment_count})`}
                             </p>
                         </li>   
                 
