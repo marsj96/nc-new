1464f0ec75e17902aa0b801e3b6e120359947d57
Jack <jack.oliver.marsden@gmail.com>
2021-11-25 11:43:04 +0000

    adds basic styling and layout for allTopics

--- src/components/AllTopics.jsx
+++ src/components/AllTopics.jsx
@@ -1,6 +1,35 @@
+import { Link } from "react-router-dom"
+import { useEffect, useState } from "react/cjs/react.development"
+import { getAllTopics } from "../utils/getAllTopics"
+
 const AllTopics = () => {
+
+    const [topics, setTopics] = useState([])
+
+    useEffect(()=>{
+        getAllTopics()
+        .then((response)=>{
+            setTopics(response)
+        })
+    }, [])
+
     return (
-        <h1> All topics </h1>
+        <div>
+            {topics.map((topic)=>{
+                return (
+                    <ul className="articlesCards">
+                        <Link to={`/topics/${topic.slug}`} style={{ textDecoration: 'none', color: 'white' }}>
+                        <li>
+                            <h2 className="articleTitle"> {topic.slug} </h2>
+                        </li>
+                        <li>
+                            <p className="articleBody"> {topic.description} </p>
+                        </li>
+                        </Link>
+                    </ul>
+                )
+            })}
+        </div>
     )
 }
 
